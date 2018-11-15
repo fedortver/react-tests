8a4606e9191e8075f346d6a029d493a61b9f7f19
fedortver <fedortver1488@gmail.com>
2018-11-15 09:06:53 +0300

    lesson 17 end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Car from './Car/Car'
+import Car from './Car'
 
 class App extends Component {
 	state={
@@ -19,14 +19,28 @@ class App extends Component {
 		pageTitle:'React components'
 	}
 
+	changeTitleHandler = (newTitle)=>{
+		this.setState({
+			pageTitle:newTitle
+		})
+	}
+
+	handleInput = (event) =>{
+		this.setState({
+			pageTitle:event.target.value
+		})
+	}
+
   render() {
 	  const cars=this.state.cars;
     return (
-      <div className="App">
+		<div className="App">
+		<button onClick={this.changeTitleHandler.bind(this,"changed")}>Change title</button>
+		<input type="text" onChange={this.handleInput}/>
 		  <h1>{this.state.pageTitle}</h1>
-		  <Car name={cars[0].name} year={cars[0].year}/>
-		  <Car name={cars[1].name} year={cars[1].year}/>
-		  <Car name={cars[2].name} year={cars[2].year}/>
+		  <Car name={cars[0].name} year={cars[0].year} onChangeTitle={this.changeTitleHandler.bind(this,cars[0].name)}/>
+		  <Car name={cars[1].name} year={cars[1].year} onChangeTitle={()=>this.changeTitleHandler(cars[1].name)}/>
+		  <Car name={cars[2].name} year={cars[2].year} onChangeTitle={()=>this.changeTitleHandler(cars[2].name)}/>
 
       </div>
     );
--- src/Car.js
+++ src/Car.js
@@ -5,6 +5,7 @@ export default (props) =>{
 		<div>
 		<h3>Car name: {props.name}</h3>
 		<p>Year: {props.year}</p>
+		<button onClick={props.onChangeTitle}>Click</button>
 		{props.children}
 	</div>
 	)
